6fbd9ee2e591fdfe7ce0fa99cbf7f0e6bd6d288a
Nora-Abotaleb <135516558+Nour-Abotaleb@users.noreply.github.com>
2024-09-03 01:08:15 +0300

    Update 2024_08_31_173733_create_notifications_table.php

--- database/migrations/2024_08_31_173733_create_notifications_table.php
+++ database/migrations/2024_08_31_173733_create_notifications_table.php
@@ -12,13 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('notifications', function (Blueprint $table) {
-            Schema::create('notifications', function (Blueprint $table) {
+
                 $table->id();
                 $table->foreignId('user_id')->constrained('users')->onDelete('cascade'); 
                 $table->string('type'); // new job post, new applications, ...
                 $table->text('message'); // When click to open this notification
                 $table->timestamps();
-            });
             
         });
     }
